de9a2e03252bb2a1edde326a6bbeebb0c55a8c68
Josh <joshuamoye732@gmail.com>
2023-04-23 14:11:54 -0500

    Got the users table working and ways to test it a bit ago but finally updated it

---
+++ SQLquery.sql
@@ -0,0 +1,13 @@
+CREATE TABLE user(
+UserId int,
+FirstName varchar(25),
+LastName varchar(25),
+Password varchar(25),
+BillingAddress varchar(55),
+ShippingAddress varchar(55),
+DateOfBirth varchar(25),
+CardNumber varchar(35),
+SecurityNumber varchar(25),
+PRIMARY KEY(UserId)
+)
+
