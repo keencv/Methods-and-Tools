c6ccf0e127aa488ec9eff74762a18aeaa5e11d85
Josh <joshuamoye732@gmail.com>
2023-04-23 21:50:10 -0500

    I hope you guys dont work on this at the last min, it will be pain

--- Checkout.py
+++ Checkout.py
@@ -12,11 +12,21 @@ except:
     sys.exit()
 cursor = connection.cursor()
 
+## where it only views the current login persons cart
+def viewcheckout(Mainid):
+    cursor.execute(f"SELECT * FROM cart WHERE UserId={Mainid}")
+    records = cursor.fetchall()
 
+    for x in records:
+       ##funny copied code
+        print("ItemId:", x[1])
+        print("ItemName:", x[2])
+        print("ItemDescription:", x[3])
+        print("ItemQuantity:", x[4])
+        print("ItemCost:", x[5])
+        print("\n")
 
-
-
-
-
+nume = 312
+viewcheckout(nume)
 cursor.close()
 connection.close()
