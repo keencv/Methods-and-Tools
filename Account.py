55c2ce38f5136052017da93e18fdda4b2eb1f2fa
Jared Doby <119348501+TheDauber@users.noreply.github.com>
2023-04-27 16:28:53 -0500

    Moved the other files into one

--- Account.py
+++ Account.py
@@ -8,6 +8,53 @@ except:
     sys.exit()
 cursor = connection.cursor()
 
+def login():
+    userinput = input("What is your User ID? \n")
+    cursor.execute(f"SELECT * FROM user WHERE UserID ={userinput}")
+    userlist = cursor.fetchall()
+    for x in userlist:
+        passwordinput = input("What is your password? \n")
+        if (passwordinput = x[3]):
+            print("Logged in\n")
+            return True
+        else:
+            print("Incorrect user ID or password\n")
+            return False
+
+def createAccount():
+    try:
+        connection = sqlite3.connect("Workbase.db")
+    
+    except:
+        print("Connection does not work")
+        sys.exit()
+        
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM user WHERE UserID != -1")
+    userlist = cursor.fetchall()
+    i = 0
+    for x in userlist:#increments i to make new unique id
+        i++
+    
+    print("Welcome to account creation\n")
+    
+    fname = input("Please enter your first name\n")
+    lname = input("Please enter your last name\n")
+    password = input("Please enter password\n")
+    bil = input("Please enter your billing address\n")
+    ship = input("Please enter your shipping address\n")
+    dob = input("Please enter your date of birth\n")
+    cnum = input("Please enter your credit or debit card number\n")
+    csv = input("Please enter your card security number\n")
+    
+    
+    cursor.execute("INSERT INTO user (UserId, FirstName, LastName, Password, BillingAddress, ShippingAddress, DateOfBirth, CardNumber, SecurityNumber) VALUES(?,?,?,?,?,?,?,?,?)", (i, fname, lname,Password,bil,ship,dob,cnum,csv))
+    connection.commit()
+    print("Account created\n")
+    cursor.close()
+    connection.close()
+    return i #returns UserId        
+
 class Account:
     def __init__(self, userID):
         self.userID = userID
